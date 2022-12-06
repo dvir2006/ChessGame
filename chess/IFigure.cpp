8989d4c0c11e322eeeec2299dd6fdda4be23995e
Bar Aharoni <aharonibar1@gmail.com>
2022-12-06 14:54:39 +0200

    added figure class

--- chess/IFigure.cpp
+++ chess/IFigure.cpp
@@ -1,1 +1,11 @@
 #include "IFIgure.h"
+
+//CTOR
+IFigure::IFigure(const int team, const std::string place)
+{
+	this->_stepsTaken = 0;
+	this->_team = team;
+	this->_place = place;
+}
+
+IFigure::~IFigure() {}
--- chess/IFigure.h
+++ chess/IFigure.h
@@ -1,5 +1,20 @@
 #pragma once
-class IFIgure
+#include <string>
+
+class IFigure
 {
+public:
+	//CTOR
+	IFigure(const int team, const std::string place);
+	//DTOR
+	virtual ~IFigure();
+
+	virtual void move(std::string dst) = 0;
+	virtual bool checkValidMove()const = 0;
+
+protected:
+	int _stepsTaken;
+	int _team;
+	std::string _place;
 };
 
