f58468158f5de66ced3c9c1066f3b578fb8fba05
Dvir Profus <dvir7002@gmail.com>
2022-12-10 10:49:39 +0200

    added checkValidMove for class queen

--- chess/Queen.cpp
+++ chess/Queen.cpp
@@ -8,5 +8,34 @@ Queen::~Queen() { }
 
 int Queen::checkValidMove(const int index,  IFigure* (&board)[BOARD_LENGTH][BOARD_LENGTH], int currTeam)const
 {
-	return 0;
+	int i = index / BOARD_LENGTH, j = index % BOARD_LENGTH, i1 = this->_place / BOARD_LENGTH, j1 = this->_place % BOARD_LENGTH, ci, cj;
+	ci = i > i1 ? 1 : -1;
+	cj = j > j1 ? 1 : -1;
+	if (index < 0 || index >BOARD_LENGTH * BOARD_LENGTH - 1)
+		return BAD_MOV_WRONG_INDEX;
+	else if (currTeam != this->_team)
+		return BAD_MOV_SRC_OP;
+	else if (board[i][j] != nullptr && board[i][j]->getTeam() == currTeam)
+		return BAD_MOV_DST_SP;
+	else if (this->_place == index)
+		return BAD_MOV_SAME_INDEX;
+	else if (abs(i - i1) != abs(j - j1) || i != i1 || j != j1)
+		return BAD_MOV_WRONG_MOV;
+	if (i1 == i)
+		for (int j2 = j1; j2 != j; j2 += cj)
+			if (board[i1][j2] != nullptr)
+				return BAD_MOV_WRONG_MOV;
+	if (j1 == j)
+		for (int i2 = i1; i2 != i; i2 += ci)
+			if (board[i2][j1] != NULL)
+				return BAD_MOV_WRONG_MOV;
+	for (i1; i < i1; i += ci)
+	{
+		for (j1; j < j1; j += cj)
+		{
+			if (board[i1][j1] != nullptr)
+				return BAD_MOV_WRONG_MOV;
+		}
+	}
+	return VALID_MOV;
 }
